b9d5317c53518dcb7a1520f090f29ff625fe35ee
RedManc76 <128239617+RedManc76@users.noreply.github.com>
2024-05-10 17:40:23 +0100

    Add files via upload

--- Scripts/preload.js
+++ Scripts/preload.js
@@ -1,7 +1,7 @@
 if (window.innerWidth >= 769) {
     var preloadLink = document.createElement('link');
     preloadLink.rel = 'preload';
-    preloadLink.href = './Images/explore.jpg';
+    preloadLink.href = './Images/main/explore.jpg';
     preloadLink.as = 'image';
     preloadLink.media = 'screen and (min-width: 769px)';
     document.head.appendChild(preloadLink);
